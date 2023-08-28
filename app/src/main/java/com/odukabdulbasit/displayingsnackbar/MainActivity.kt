723d1edb4fac28cef5e65c12e90c199e899abe31
Abdulbasit Oduk <a.oduk@pegb.tech>
2023-08-28 12:38:37 +0300

    Initial commit

---
+++ app/src/main/java/com/odukabdulbasit/displayingsnackbar/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.odukabdulbasit.displayingsnackbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, SnackbarFragment())
+                .commit()
+        }
+    }
+}
